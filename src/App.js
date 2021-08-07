1869e8a5d4a0259b25465e7b81ef6553243b728b
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-08-07 13:58:12 -0500

    global style componet running

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-<div>
-  
-</div>
+<AppStyled>
+  <h1> project portfolio </h1>
+</AppStyled>
   );
 }
 
+
+const AppStyled = styled.div`
+background-color: blue;
+`;
 export default App;
