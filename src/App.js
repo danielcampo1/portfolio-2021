9d84d630accf00dbbe2c6159c8819710c63cc900
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-08-10 20:12:26 -0500

    created base outline of main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import SideBar from './components/sideBar';
+import styled from 'styled-components';
+
 
 function App() {
   return (
-<div>
-  <SideBar />
-</div>
+      <div className="main-content">
+
+      <SideBar />
+
+        <MainContentSyled>
+
+        </MainContentSyled>
+      </div>
+    
   );
 }
+
+const MainContentSyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+`;
 
 export default App;
