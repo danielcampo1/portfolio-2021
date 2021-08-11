f8ace730277bb308eeb85b0bea16517f5ddb7593
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-08-10 20:12:37 -0500

    created home page comp

---
+++ src/pages/homePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function homePage() {
+    return (
+       <HomePageStyled>
+
+       </HomePageStyled>
+    )
+}
+
+HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+`;
+
+export default homePage
+
+
