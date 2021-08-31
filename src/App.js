0797b2e53a7be918074b9a3b4573a8fa056b2650
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-08-31 06:04:29 -0600

    created floating particle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import SideBar from './components/sideBar';
 import styled from 'styled-components';
+import HomePage from './pages/homePage';
 
 
 function App() {
@@ -10,6 +11,14 @@ function App() {
       <SideBar />
 
         <MainContentSyled>
+          <div className="lines">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+            <div className="line-3"></div>
+            <div className="line-4"></div>
+          </div>
+
+        <HomePage />
 
         </MainContentSyled>
       </div>
@@ -21,6 +30,19 @@ const MainContentSyled = styled.main`
   position: relative;
   margin-left: 16.3rem;
   min-height: 100vh;
+
+  .lines{
+    position: absolute;
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    .line-1, .line-2, .line-3, .line-4{
+      width: 1px;
+      min-height: 100vh;
+      background-color: var(--border-color);
+    }
+  }
 `;
 
 export default App;
